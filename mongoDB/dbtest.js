edac79ae92434a221a8633eca88a8cea8d80c517
JackSpaGenZ <jackspa0709@gmail.com>
2023-03-31 17:02:08 +0700

    2rd commit

--- mongoDB/dbtest.js
+++ mongoDB/dbtest.js
@@ -1,4 +1,5 @@
 var MongoClient = require('mongodb').MongoClient;
+//Create a database named "mydb":
 var url = "mongodb://localhost:27017/mydb";
 
 MongoClient.connect(url, function(err, db) {
---
+++ test.js
@@ -0,0 +1,1 @@
+console.log("hello anh em")
